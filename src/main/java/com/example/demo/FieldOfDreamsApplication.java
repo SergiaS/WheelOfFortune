581c4a9f60a8e1068d9dd0cf2937131e000972c5
sergias <sergias@ukr.net>
2021-02-08 12:47:07 +0200

    init commit

---
+++ src/main/java/com/example/demo/FieldOfDreamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldOfDreamsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FieldOfDreamsApplication.class, args);
+    }
+
+}
